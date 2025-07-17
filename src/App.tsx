30661d446199863400c298f33d7abfd4e446af55
DanAtWork13 <daniel.silverstein@onehomesolution.com>
2025-07-17 10:52:26 -0500

    function test 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,26 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Button } from "@/components/ui/button";
 //import "./App.css";
 
 function App() {
-	const [count, setCount] = useState(0);
+	const [count, setCount] = useState("Click me");
+
+	async function runRequest(): string {
+		const response = await fetch("/api/hellorld");
+		const data = await response.json();
+		return data.message;
+	}
+
 
 	return (
 		<>
-			<div>
-				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
+			<h1>Vercel Functions test</h1>
 			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+				<Button onClick={() => setCount(runRequest())}>{count}</Button>
 				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
+					Click to test function
 				</p>
 			</div>
-			<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
 		</>
 	);
 }
